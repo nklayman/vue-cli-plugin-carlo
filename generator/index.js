3aeb2054c9c28d9af0c17494315e48e961f867ec
Noah Klayman <noahklayman@gmail.com>
2018-11-12 14:28:07 -0800

    feat(generator): automatically add necessary folders to .gitignore

--- generator/index.js
+++ generator/index.js
@@ -1,5 +1,19 @@
+const fs = require('fs')
+
 module.exports = api => {
   api.render('./template')
+
+  api.onCreateComplete(() => {
+    // Update .gitignore if it exists
+    if (fs.existsSync(api.resolve('./.gitignore'))) {
+      let gitignore = fs.readFileSync(api.resolve('./.gitignore'), 'utf8')
+      if (!/(#Carlo build output|\/dist_carlo|\/\.profile)/.test(gitignore)) {
+        // Add /dist_carlo and /.profile to gitignore if it doesn't exist already
+        gitignore += '\n#Carlo build output\n/dist_carlo\n/.profile'
+        fs.writeFileSync(api.resolve('./.gitignore'), gitignore)
+      }
+    }
+  })
 
   api.extendPackage({
     scripts: {
